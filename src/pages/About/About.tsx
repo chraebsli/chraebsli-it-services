3d516710db3238654806e71fe7af11c260db5b1e
chraebsli <nicholas.joel.krebs@gmail.com>
2023-05-30 22:36:36 +0200

    removed unnecessary sass

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card, Stack, Typography } from "@mui/material";
 import Page from "../../components/common/Page";
-import "./About.sass";
 import PersonIcon from "@mui/icons-material/Person";
 import { useTranslation } from "react-i18next";
 
